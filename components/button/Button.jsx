6aa670833fdd14575aaf2fa6380098debc182cf0
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-08-25 02:11:20 +0500

    asd

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Button({text, classes, id, link, duration}) {
     return (
-        <Link href={link ? link : "#"}>
+        <Link href={link ? link : "/#"}>
             <button data-aos="fade-up" data-aos-duration={duration ? duration : "0"} id={id}
                     className={`${classes} rounded-2xl border hover:opacity-90 text-white py-3.5 md:px-14 px-8`}>{text}</button>
         </Link>
