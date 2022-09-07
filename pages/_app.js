9f95d96ef5b5a40a0a3b571993906b27d2546e7a
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-07 23:16:04 +0500

    some changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-ZGXQT0Y6WD"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZGXQT0Y6WD');
+        `}
+        </Script>
+
+        <Component {...pageProps} />
+      </>
+      )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,6 @@ export default function Home() {
             <meta name="description" content="Mobile & web software development company"/>
             <link rel="icon" href="/Logo.svg"/>
             <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
-            {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-            <Script async={true} src="https://www.googletagmanager.com/gtag/js?id=G-ZGXQT0Y6WD"
-                    strategy={"lazyOnload"}/>
-            <Script id={"sadf"} strategy="lazyOnload">
-                {`window.dataLayer = window.dataLayer || []; 
-            function gtag(){dataLayer.push(arguments)}; 
-            gtag('js', new
-            Date()); gtag('config', 'G-ZGXQT0Y6WD');`}
-            </Script>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-10933023663"/>
-            <Script id={"as"} strategy="lazyOnload">
-                {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-10933023663');`}
-            </Script>
         </Head>
         <Navbar/>
         <main>
