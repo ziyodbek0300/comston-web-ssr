50300cc8b40ab1fe033d03f8ac1f89c4d98431ae
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-07 23:29:03 +0500

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
 import '../styles/globals.css'
 import Script from "next/script";
+import {useRouter} from 'next/router'
+import * as gtag from '../lib/gtag'
+import {useEffect} from "react";
 
-function MyApp({ Component, pageProps }) {
-  return(
-      <>
+function MyApp({Component, pageProps}) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            console.log("handle")
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        router.events.on('hashChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+            router.events.off('hashChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
+    return (<>
         <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-ZGXQT0Y6WD"
             strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {`
+            {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -19,8 +36,7 @@ function MyApp({ Component, pageProps }) {
         </Script>
 
         <Component {...pageProps} />
-      </>
-      )
+    </>)
 }
 
 export default MyApp
