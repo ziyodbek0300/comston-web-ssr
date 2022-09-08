c59c106b473df4ec9340d75ecc601eef441f7dfe
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-08 16:44:09 +0500

    some changes

--- pages/app-development/FirstSection.jsx
+++ pages/app-development/FirstSection.jsx
@@ -4,14 +4,14 @@ import Button from "/components/button";
 function FirstSection() {
     return (<div className={"bg-blue-150 first-section-app flex lg:items-center lg:text-left text-center pb-16"}>
         <div className={"container flex lg:flex-row flex-col"}>
-            <div className={"pt-10 lg:w-[45%] w-full pb-10"}>
-                <h3 data-aos={"fade-up"} data-aos-duration={"800"} className={"text-40-56-700 uppercase lg:w-full w-[90%] mx-auto mb-6 mt-14"}>
+            <div className={"md:pt-10 lg:w-[45%] w-full pb-10"}>
+                <h3 data-aos={"fade-up"} data-aos-duration={"800"} className={"text-40-56-700 uppercase lg:w-full md:w-[90%] mx-auto mb-6 md:mt-14 mt-10"}>
                 Full-cycle <span className={"text-blue-550"}>mobile app
                     development</span> company
                     in {""}
                     <span className={"text-green-550"}>Florida</span>
                 </h3>
-                <p data-aos={"fade-up"} data-aos-duration={"1000"} className={"text-16-24-400 mb-10 lg:w-full w-2/3 mx-auto"}>
+                <p data-aos={"fade-up"} data-aos-duration={"1000"} className={"text-16-24-400 mb-10 lg:w-full md:w-2/3 m-0 mx-auto"}>
                     Fulfill your business objectives with creative solutions
                     through robust and cost-effective
                     app development methods
--- pages/web-development/FirstSection.jsx
+++ pages/web-development/FirstSection.jsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 function FirstSection() {
     return (<div className={"bg-blue-150 first-section-web flex items-center"}>
-        <div className={"container flex lg:flex-row flex-col lg:text-left text-center py-[100px] px-[20px]"}>
-            <div className={"pt-10 lg:w-[45%] w-full"}>
-                <h3 data-aos={"fade-up"} data-aos-duration={"800"} className={"text-40-56-700 capitalize mb-4 lg:w-full w-[90%] mx-auto"}>
+        <div className={"container flex lg:flex-row flex-col lg:text-left text-center md:py-[100px] px-[20px]"}>
+            <div className={"md:pt-10 pt-32 lg:w-[45%] w-full"}>
+                <h3 data-aos={"fade-up"} data-aos-duration={"800"} className={"text-40-56-700 capitalize mb-4 lg:w-full md:w-[90%] mx-auto"}>
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
                     <span className={"text-blue-550"}>RESPONSIVE & PROFESSIONAL
                 </span> <br/>WEBSITE DEVELOPMENT
                 </h3>
-                <p data-aos={"fade-up"} data-aos-duration={"1000"} className={"text-16-24-400 mb-10 lg:w-full w-2/3 mx-auto"}>
+                <p data-aos={"fade-up"} data-aos-duration={"1000"} className={"text-16-24-400 mb-10 lg:w-full md:w-2/3 mx-auto"}>
                     Our vetted website developers will create a customized website to
                     achieve all of your business goals
                 </p>
