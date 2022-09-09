fae45cdc8d6c993b5acff09ed04d8659785f2e3d
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-09 14:28:47 -0400

    email

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
             var callback = function () {
                 if (typeof (url) != 'undefined') {
                     window.location = url;
-                } else if (url === 'http://localhost:3000') {
+                } else if (url === 'http://www.comston.io') {
                     window.location = url;
                 }
             };
