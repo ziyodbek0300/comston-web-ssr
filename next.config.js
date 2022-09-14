e95cf57a4a33a62fc8ddd7950700b2fbdd30b0b9
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-14 19:09:03 -0400

    content changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+    unoptimized: true
+  },
+
 }
 
 module.exports = nextConfig
