af7a136f746b97cc1d4ef01d2d653ca3ab370c58
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-19 21:25:53 +0500

    some changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        dangerouslyAllowSVG: true,
+        path: '/_next/image',
+        disableStaticImages: false,
+        unoptimized: true
+    }
 }
 
 module.exports = nextConfig
